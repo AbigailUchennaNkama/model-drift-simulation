9d42789ca315cee1d59321f27b9c3ccf7ee87cf1
Abigail <110779467+AbigailUchennaNkama@users.noreply.github.com>
2023-09-13 18:10:15 +0200

    Delete drft_simulation_modules directory

--- drft_simulation_modules/get_data.py
+++
@@ -1,8 +0,0 @@
-from torchvision.datasets.utils import download_url
-import tarfile
-
-dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz "
-download_url(dataset_url, '.')
-
-with tarfile.open("/content/cifar10.tgz ", "r:gz") as tar:
-    tar.extractall(path="./data")

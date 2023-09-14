19947059a4744b57529526edf54a72fefc67929c
AbigailNkama <nkamaabigail1@mail.com>
2023-09-14 03:04:58 +0200

    updated model data

--- drift_modules/plot.py
+++ drift_modules/plot.py
@@ -10,4 +10,6 @@ def show_image(dataloader):
   img = make_grid(img, nrow=12)
   img = img / 2 + 0.5     # unnormalize
   npimg = img.numpy()     #convert to numpy
+  plt.imshow(np.transpose(npimg, (1, 2, 0)))
+  print(' '.join(f'{class_names[labels[j]]:5s}' for j in range(batch_size)))
   plt.show()

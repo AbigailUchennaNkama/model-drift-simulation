37e2879d297fecfd26c6eb8a43188a52da05c3af
AbigailNkama <nkamaabigail1@mail.com>
2023-09-19 12:14:17 +0200

    added accuracy function

---
+++ drift_modules/model_accuracy.py
@@ -0,0 +1,13 @@
+def accuracy_fn(y_true, y_pred):
+    """Calculates accuracy between truth labels and predictions.
+
+    Args:
+        y_true (torch.Tensor): Truth labels for predictions.
+        y_pred (torch.Tensor): Predictions to be compared to truth labels.
+
+    Returns:
+        [torch.float]: Accuracy value between y_true and y_pred, e.g. 78.45
+    """
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct / len(y_pred)) * 100
+    return acc

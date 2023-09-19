8fac3011b01b4cce4797c963c6a72653c94c7abf
AbigailNkama <nkamaabigail1@mail.com>
2023-09-20 00:03:48 +0200

    updated get_all_data

--- drift_modules/get_all_data.py
+++ drift_modules/get_all_data.py
@@ -4,32 +4,34 @@ from pathlib import Path
 
 from torchvision.datasets.utils import download_url
 import tarfile
-print("creating cifar10 dataset...")
-dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz "
+
+print("Creating cifar10 dataset...")
+dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz"
 download_url(dataset_url, '.')
-with tarfile.open("./cifar10.tgz ", "r:gz") as tar:
+
+with tarfile.open("./cifar10.tgz", "r:gz") as tar:
     tar.extractall(path="./data")
 
-print("creating cifar09 dataset...")
+print("Creating cifar9 dataset...")
 data_dir = "./data/cifar10"
-test_data_paths = list(Path(data_dir+"/train").glob("*/*.png"))
+train_data_paths = list(Path(data_dir + "/train").glob("*/*.png"))
 
 # Exclude the "dog" class
-test_data_paths = [path for path in test_data_paths if path.parent.stem != "truck"]
-test_labels = [path.parent.stem for path in test_data_paths]
+train_data_paths = [path for path in train_data_paths if path.parent.stem != "dog"]
 
 data_dir = "./data/cifar9"
 train_dir = os.path.join(data_dir, "train")
+
 # Define the classes to include (excluding "dog")
-included_classes = ['airplain','automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship']
+included_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'frog', 'horse', 'ship', 'truck']
 
 # Create subfolders for included classes
 for class_name in included_classes:
     class_dir = os.path.join(train_dir, class_name)
     os.makedirs(class_dir, exist_ok=True)
 
-# List all files in the test directory (using test_data_paths)
-all_files = list(test_data_paths)
+# List all files in the train directory (using train_data_paths)
+all_files = list(train_data_paths)
 
 # Move files to their respective subfolders
 for file_path in all_files:
@@ -37,4 +39,5 @@ for file_path in all_files:
     if class_name in included_classes:
         dest_dir = os.path.join(train_dir, class_name)
         shutil.copy(str(file_path), dest_dir)
-print("Images organized into subfolders based on class names while keeping copies.")
+
+print("Images organized into subfolders based on class names.")
